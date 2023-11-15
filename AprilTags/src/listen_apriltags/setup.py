2ee0fee99aa29ffea4e43e83ab6e3ab6bdb0265d
Courtney Smith <courtney77smith77@gmail.com>
2023-11-14 20:41:18 -0600

    created launch file for camera transform node launchfile starts: realsense, image proc, apriltag_node, listen_apriltags node, and rviz

--- AprilTags/src/listen_apriltags/setup.py
+++ AprilTags/src/listen_apriltags/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 'launch/aprilTags.launch.xml', 'config/tags.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'listen = listen_apriltags.listening:main'
+            'listener = listen_apriltags.listening:main'
         ],
     },
 )
